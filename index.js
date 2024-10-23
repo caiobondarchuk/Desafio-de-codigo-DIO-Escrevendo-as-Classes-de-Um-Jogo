d85cac02b4ba3f14cb2ecb40b82f944cfd3d725a
caiobondarchuk <caiobondarchuk0@gmail.com>
2024-10-22 21:07:08 -0300

    Adicionando codigo

---
+++ index.js
@@ -0,0 +1,38 @@
+class heroi{
+    constructor(nome, idade, tipo){
+        this.nome = nome
+        this.idade = idade
+        this.tipo = tipo
+
+    }
+    
+    atacar(){
+        let ataque = ""
+
+        switch (this.tipo) {
+            case "guerreiro":
+                ataque = "espada"
+                break;
+            case "mago":
+                ataque = "magia"
+                break;
+            case "monge":
+                ataque = "artes marciais"
+                break;
+            case "ninja":
+                ataque = "shuriken"
+                break;
+        
+            default:
+                ataque = "(nenhum ataque encontado para esse tipo)"
+                break;
+        }
+
+        console.log(`o ${this.tipo} atacou usando ${ataque}`);
+        
+    }
+
+}
+
+let dadosHeroi = new heroi("Caio", "16", "viking");
+dadosHeroi.atacar();
